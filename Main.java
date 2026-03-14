bea6ca595fb235fddc33f5d36bb051b1c0979b38
Bhupesh18k <bhupesh.kumar18k@gmail.com>
2026-03-14 10:55:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,70 +1,40 @@
-import java.util.*;
-public class Main {
-    public static int[] mergeSort(int[] arr, int lo, int hi) {
-        if (lo == hi) {
-            int[] ba = new int[1];
-            ba[0] = arr[lo];
-            return ba;
-        }
-        int mid = (lo + hi) / 2;
-        int[] fsh = mergeSort(arr, lo, mid);
-        int[] ssh = mergeSort(arr, mid + 1, hi);
-
-        int[] fsa = mergeTwoSortedArrays(fsh, ssh);
-        return fsa;
-    }
-    public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
+// TEXT JUSTIFIATION
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {  
+        List<String> result = new ArrayList<>();
         int i = 0;
-        int j = 0;
-        int k = 0;
-        int[] ans = new int[a.length + b.length];
-        while (i < a.length && j < b.length) {
-            if (a[i] <= b[j]) {
-                ans[k] = a[i];
-                i++;
-                k++;
-            } else {
-                ans[k] = b[j];
+        while(i < words.length){
+            int j = i;
+            int lineLength = 0;
+            while(j < words.length && lineLength + words[j].length() + (j - i) <= maxWidth){
+                lineLength += words[j].length();
                 j++;
-                k++;
             }
+            int gaps = j - i - 1;
+            StringBuilder line = new StringBuilder();
+            if(j == words.length || gaps == 0){
+                for(int k = i; k < j; k++){
+                    line.append(words[k]);
+                    if(k < j - 1) line.append(" ");
+                }
+                while(line.length() < maxWidth){
+                    line.append(" ");
+                }
+            }
+            else{
+                int spaces = (maxWidth - lineLength) / gaps;
+                int extra = (maxWidth - lineLength) % gaps;
+                for(int k = i; k < j; k++){
+                    line.append(words[k]);
+                    if(k < j - 1){
+                        for(int s = 0; s < spaces; s++) line.append(" ");
+                        if(extra-- > 0) line.append(" ");
+                    }
+                }
+            }
+            result.add(line.toString());
+            i = j;
         }
-
-        while (i < a.length) {
-            ans[k] = a[i];
-            i++;
-            k++;
-        }
-        while (j < b.length) {
-            ans[k] = b[j];
-            j++;
-            k++;
-        }
-        return ans;
+        return result;
     }
-    public static void print(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
-        }
-        int[] sa = mergeSort(arr, 0, arr.length - 1);
-        print(sa);
-        scn.close();
-    }
-
-
-
-
-
-
-
-
 }
